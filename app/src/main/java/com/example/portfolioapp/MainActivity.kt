98caaf8151930445a7f027b3e9b22ec735b6d17b
jayesh <jayeshthorat98@gmail.com>
2023-05-19 15:47:14 +0530

    working mainactivity

--- app/src/main/java/com/example/portfolioapp/MainActivity.kt
+++ app/src/main/java/com/example/portfolioapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.portfolioapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -15,15 +16,37 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.portfolioapp.ui.theme.PortfolioAppTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnTest1= findViewById<Button>(R.id.btnTest1)
+        val btnTest1 = findViewById<Button>(R.id.btnTest1)
 
         btnTest1.setOnClickListener {
             intent = Intent(this, Test1::class.java)
             startActivity(intent)
+
+        val btnTest2 = findViewById<Button>(R.id.btnTest2)
+
+        btnTest2.setOnClickListener {
+            intent = Intent(this, Test2::class.java)
+            startActivity(intent)
+
+        val btnTest3 = findViewById<Button>(R.id.btnTest3)
+
+        btnTest3.setOnClickListener {
+            intent = Intent(this, Test3::class.java)
+            startActivity(intent)
+
+        val btnTest4 = findViewById<Button>(R.id.btnTest4)
+
+        btnTest4.setOnClickListener {
+            intent = Intent(this, Test4::class.java)
+            startActivity(intent)
+        }
+        }
+        }
         }
         }
     }
